b517030fa3ec420dd94635cdf54317761bbdbdfb
d <d>
2024-08-25 21:27:26 -0400

    Add version to about window

--- MacOS/TooFarDidntLock/TooFarDidntLock/AboutView.swift
+++ MacOS/TooFarDidntLock/TooFarDidntLock/AboutView.swift
@@ -5,14 +5,16 @@ import OSLog
 struct AboutView: View {
     let logger = Log.Logger("AboutView")
     var body: some View {
+        let version = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String
         VStack {
             Image("AboutImage")
                 .resizable()
                 .scaledToFit()
                 .frame(width: 500, height: 500)
             Text("Too Far; Didn't Lock")
+            Text("Version \(version ?? "unknown")")
             Text("[Github](https://github.com/putgeminmouth/toofardidntlock)")
         }
-            .padding(20)
+        .frame(width: 500, height: 570)
     }
 }
